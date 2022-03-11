30e0caf3bdf20c8b4e364091f6980179d0808cfe
Batuhan Çelik <21010310060@ogrenci.bartin.edu.tr>
2022-03-11 23:03:24 +0300

    Session2_Week3
    Uygulama_Ornekleri

---
+++ Session2_Week3/Java2_Donem/src/Point2.java
@@ -0,0 +1,16 @@
+public class Point2 {
+
+    int x;
+    int y;
+
+
+public double distanceFromOrigin() {
+    return Math.sqrt(x*x + y*y);
+}
+
+public void translate(int dx, int dy) {
+    x += dx;
+    y += dy;
+}
+
+    }
---
+++ Session2_Week3/java2_Donem2/src/java2_Donem2/main.java
@@ -0,0 +1,22 @@
+package java2_Donem2;
+
+public class main {
+
+    public static void main(String[] args) {
+
+          Cat kedi1 = new Cat();
+          System.out.println(kedi1.age+" , "+kedi1.color+" , "+kedi1.name );
+
+          Cat kedi2 = new Cat("Tarcin",2);
+          System.out.println(kedi2.age+" , "+kedi2.color+" , "+kedi2.name );
+
+          Cat kedi3 = new Cat("Tekir","gri",3);
+          System.out.println(kedi3.age+" , "+kedi3.color+" , "+kedi3.name );
+
+          Cat kedi4 = new Cat(2,"Duman","gri");
+          System.out.println(kedi4.age+" , "+kedi4.color+" , "+kedi4.name );
+
+    }
+
+
+}
